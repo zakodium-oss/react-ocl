e81cf151bb5001b17e63342e5d39aaa526a15d34
Michaël Zasso <targos@protonmail.com>
2024-10-09 14:39:23 +0200

    chore: update dependencies and Storybook

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,15 +1,21 @@
-module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+export default {
+  stories: ['../stories/**/*.stories.jsx'],
+
   addons: [
     '@storybook/addon-storysource',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
   ],
+
   framework: {
-    name: '@storybook/react-webpack5',
+    name: '@storybook/react-vite',
     options: {
       fastRefresh: true,
       strictMode: true,
     },
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 };
